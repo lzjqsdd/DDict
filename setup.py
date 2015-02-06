2537903cd03e7808fb7bc1f83b4b87285349677d
lzjqsdd <13253989980@163.com>
2015-02-06 14:17:08 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name="ddict",
+    version="0.10",
+    description="mini youdao dict",
+    url="https://github.com/lzjqsdd/DDict.git",
+    license="LGPL",
+    packages=find_packages(),
+    scripts=["ddict/"]
+    )
