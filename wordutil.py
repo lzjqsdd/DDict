7b0202337219ab0818380b9482667b14a53b7ffb
lzjqsdd <13253989980@163.com>
2015-03-09 09:55:57 +0800

    add win

--- wordutil.py
+++ wordutil.py
@@ -1,5 +1,4 @@
 #coding=utf8
-import thread
 import time
 import os
 import sys
@@ -16,89 +15,67 @@ class wordutil:
         self.word='abcdefghijklmnopqrstuvwxyz'
         self.url="http://fanyi.youdao.com/openapi.do?keyfrom=youdaomini&key=1877956118&type=data&doctype=json&version=1.1&q="
 
-    def threadTest(self):
-        while self.count:
-            var = os.popen('xsel').read()
-            if(var!=self.word and var!=''):
-                self.printStart()
-                varr = self.getRequestWord(var)
-                print "  [查询]:\t",varr
-                searchurl=self.url+urllib.quote(varr)
-                ret=''
-                req = urllib2.Request(searchurl)
-                res_data = urllib2.urlopen(req)
-                ret = res_data.read()
-                if(ret==''):
-                    print 'error!'
-                else:
-                    #print ret
-                    self.getResult(ret)
-                    self.printDone(var)
-                self.word=var
-            time.sleep(0.2)
 
-    def printStart(self):
-        os.system("clear")
-        print "\t\t\033[1;31;40m%s\033[0m" % "[有道翻译fanyi.youdao.com提供]"
+    def execfind(self):
+        var = os.popen('xsel').read()
+        if(var!=self.word and var!=''):
+            varr = self.getRequestWord(var)
+            searchurl = self.url+urllib.quote(varr)
+            ret=''
+            req = urllib2.Request(searchurl)
+            res_data = urllib2.urlopen(req)
+            ret = res_data.read()
+            if(ret!=''):
+                self.word = var
+                return self.getResultForWin(var,ret)
+        return ""
 
-    def printDone(self,var):
-        var_len = len(var)
-        if var_len>50:
-            var_len=30
-        print ""
-        for i in range(0,var_len+20):
-            if(i==(var_len+20)/2):
-                sys.stdout.write("Powerd by DDict")
-            sys.stdout.write('*')
-        print ""
 
     def getRequestWord(self,word):
         text = word.split('\n')[0].strip().strip('\r\n').strip('\r\n\x00')
-        #print text
         return text
 
 
-    def getResult(self,word):
+    def getResultForWin(self,requestword,word):
         orijson = {}
+        result = "  [查询]："+"\t"+requestword+"\n"
         try:
             orijson = json.loads(word)
         except ValueError:
-            print "解析错误，请重新取词"
-            return
+            result = result + "解析错误，请重新取词"+"\n"
+            return result
 
         ret_key = orijson.keys()
         if("errorCode" in orijson):
             codenum = orijson["errorCode"]
             if(codenum==20):
-                print "翻译文本过长"
+                result = result + "翻译文本过长"+"\n"
                 return
             elif(codenum==30):
-                print "无法进行有效的翻译"
+                result = result +"无法进行有效的翻译"+"\n"
                 return
             elif(codenum==40):
-                print "不支持的语言类型"
+                result = result +"不支持的语言类型"+"\n"
                 return
             elif(codenum==50):
-                print "无效的key"
+                result = result +"无效的key"+"\n"
                 return
             elif(codenum==50):
-                print "无辞典结果，仅在获取辞典结果生效"
+                result = result +"无辞典结果，仅在获取辞典结果生效"+"\n"
                 return
         if("translation" in orijson):
             if(orijson["translation"]==orijson["query"]):
-                print "未找到合适的翻译结果"
+                result = result +"未找到合适的翻译结果"+"\n"
             else:
-                print "  [翻译]:\t",orijson["translation"][0]
+                result = result +"  [翻译]:\t"+orijson["translation"][0]+"\n"
         if("basic" in ret_key):
             if("us-phonetic" in orijson["basic"]):
-                print "  [美式发音]:\t",orijson["basic"]["us-phonetic"]
+                result = result +"  [美式发音]:\t"+orijson["basic"]["us-phonetic"]+"\n"
             if("uk-phonetic" in orijson["basic"]):
-                print "  [英式发音]:\t",orijson["basic"]["uk-phonetic"]
+                result = result +"  [英式发音]:\t"+orijson["basic"]["uk-phonetic"]+"\n"
             if("explains" in orijson["basic"]):
                 explains = orijson["basic"]["explains"]
-                print "  [词典]:"
+                result = result +"  [词典]:"+"\n"
                 for ex in explains:
-                    print "\t",ex
-
-    def run(self):
-        thread.start_new_thread(self.threadTest,())
+                    result = result +"\t"+ex+"\n"
+        return result
